53cac08460ecaa201be42d1f8eaa09257a4a395b
Dungx <nguyentiendung342000@gmail.com>
2022-02-27 18:59:29 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// chatbot
+function moForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+function dongForm() {
+    document.getElementById("myForm").style.display = "none";
+}
